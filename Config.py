cbd567f579c50e771ae524d987a56b3c17ba4c5b
szymon <szymon.janas.95@gmail.com>
2020-05-06 14:39:55 +0200

    Add: Config module

--- Config.py
+++ Config.py
@@ -1,10 +1,9 @@
 import json
+import os
 
-class Config:
-    def __init__(self):
-        self.data = None
-        with open('c:/Users/szymo/OneDrive/Documents/music_visualisation/rt_spectrum_analyzer/config.json') as json_data_file:
-            self.data = json.load(json_data_file)
+data = None
+with open("config.json") as json_data_file:
+    data = json.load(json_data_file)
 
-    def get_settings(self, arg: str):
-        return self.data[arg]
+def get_settings(arg: str):
+    return data[arg]
