71d29daad0194d02c9859ebb6fb39272f2c0002c
szymon <szymon.janas.95@gmail.com>
2020-05-06 16:05:27 +0200

    Add: models, and visualization

--- Visualisation.py
+++ Visualisation.py
@@ -9,14 +9,15 @@ from scipy.fftpack import fft
 import sys
 import time
 
-from Signal import Audio, Signal
-from SignalProcessing import SignalProcessing
+import Signal
+import Convert
+import BarsAlgorithms
+import SpectrumProcessing
 
 class AudioStream(object):
     def __init__(self):
 
-        self.signal = Audio()
-        self.algorithms = SignalProcessing()
+        self.signal = Signal.get_audio()
 
         # pyqtgraph stuff
         pg.setConfigOptions(antialias=True)
@@ -27,25 +28,31 @@ class AudioStream(object):
         self.win.setGeometry(400, 250, 600, 300)    
 
         self.signalPlot = self.win.addPlot(
-            title='WAVEFORM', row=1, col=1
+            title='WAVEFORM', row=1, col=2
         )
-        sensivity = 8
+        sensivity = 10
         self.signalPlot.setYRange(-10**sensivity, 10**sensivity, padding=0.05)
         self.signalPlot.setXRange(0, 2048-1, padding=0.05)
         self.plotSignal = self.signalPlot.plot(pen='c', width=3)
         
         self.spectrumPlot = self.win.addPlot(
-            title='SPECTRUM', row=2, col=1
+            title='SPECTRUM', row=1, col=1
         )
         self.spectrumPlot.setYRange(0, 10**(sensivity+2))
         self.spectrumPlot.setXRange(0, 20000)
         self.plotSpectrum = self.spectrumPlot.plot(pen='m', width=3)
 
         self.spectrumPlotBar = self.win.addPlot(
-            title='SPECTRUM BARS', row=3, col=1
+            title='SPECTRUM BARS', row=2, col=1
         )
-        self.spectrumPlotBar.setYRange(0, 10**(sensivity+1))
+        self.spectrumPlotBar.setYRange(-1, 110)
         self.plotSpectrumBar = self.spectrumPlotBar.plot(pen='r', width=3, stepMode=True)
+
+        self.spectrumPlotBar2 = self.win.addPlot(
+            title='SPECTRUM BARS ORG', row=2, col=2
+        )
+        self.spectrumPlotBar2.setYRange(0, 10**(sensivity+1))
+        self.plotSpectrumBar2 = self.spectrumPlotBar2.plot(pen='r', width=3, stepMode=True)
 
         self.item = pg.PlotDataItem()
         
@@ -67,11 +74,15 @@ class AudioStream(object):
         return [x, yy]
 
     def set_plotdata(self):
-        sig = self.signal.get_signal()
-        self.plotSignal.setData(sig.signal_timeline, sig.signal_data)
-        self.plotSpectrum.setData(sig.fourier_freq, sig.fourier_data)
-        data = self.convert_to_qt__bar_chart(self.algorithms.convert_spectrum_to_bars_v2(sig.fourier_data.tolist()))
+        sig = Signal.get_signal()
+        self.plotSignal.setData(sig.samples_timeline, sig.samples)
+        self.plotSpectrum.setData(sig.spectrum_freq, sig.spectrum)
+
+        data = self.convert_to_qt__bar_chart(SpectrumProcessing.process_spectrum(sig.spectrum))
         self.plotSpectrumBar.setData(data[0], data[1])
+
+        data2 = self.convert_to_qt__bar_chart(SpectrumProcessing.process_spectrum_v2(sig.spectrum))
+        self.plotSpectrumBar2.setData(data2[0], data2[1])
 
     def update(self):
         self.set_plotdata()
